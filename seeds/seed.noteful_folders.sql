33a68a2a58f791b8bac604c277613e2928d06f15
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-02-27 21:33:37 -0800

    added first CRUD operations for folders

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO folders (name, id)
+VALUES
+('Important', 1),
+('Super', 2),
+('Spangley', 3);
+
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO notes (id, name, content, folder, date_modified)
+VALUES
+(1, 'Dogs', 'Content1', 1, '2019-01-03T00:00:00.000Z'),
+(2, 'Cats', 'Content2', 2, '2019-01-03T00:00:00.000Z'),
+(3, 'Birds', 'Content3', 1, '2019-01-03T00:00:00.000Z'),
+(4, 'Fish', 'Content4', 3, '2019-01-03T00:00:00.000Z'),
+(5, 'Bears', 'Content5', 1, '2019-01-03T00:00:00.000Z');
